7c8497d7820d8c9cf08b6b1d41fd6fc2820d7969
ji64 <ji64@njit.edu>
2020-10-18 21:09:11 -0400

    Updating website

--- home.php
+++ home.php
@@ -15,4 +15,16 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 }
 ?>
 <p>Welcome, <?php echo $email; ?></p>
+<style>
+    body{
+        background-color: #E5E5E5;
+        margin: 0 0;
+        padding: 0 0;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    p{
+        text-align: center;
+    }
+</style>
 <?php require(__DIR__ . "/partials/flash.php");
